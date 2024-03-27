11e399ac00705cd4755661d48bdf455f9fb94456
islombek-developer <164884102+islombek-developer@users.noreply.github.com>
2024-03-27 16:47:32 +0300

    1-git fayilim

---
+++ index.py
@@ -0,0 +1,33 @@
+# def lovefunc( flower1, flower2 ):
+#     return (flower1+flower2)%2
+# print(lovefunc(6,5))
+# def digitize(n):
+#     return [int(i) for i in str(n)[::-1]]
+# a=35698         
+# print(digitize(a))  
+
+# def abbrevName(name):
+#     return '.'.join(w[0] for w in name.split()).upper()
+        
+# a="Islombek Botiraliyev"
+# print(abbrevName(a))
+# def litres(time):
+#     return int(time/2)
+# a=3
+# print(litres(a))
+# Birinchi asr 1- yildan 100- yilgacha , ikkinchi asr 101-yildan 200-yilgacha va hokazolarni oʻz ichiga oladi.
+# def f(x):
+#     return min(x)
+# a=[1,2,3,8,9,5,-2]
+# print(f(a))
+# def odd_ones_out(numbers):
+#     result = []
+#     for num in set(numbers):
+#         if numbers.count(num) % 2 == 0:
+#             result.extend([num] * (numbers.count(num)))
+#     return result
+    
+# print(odd_ones_out([1,4,4, 2, 3, 1, 3, 3,3,2]))  
+
+
+print("salom")
